2e17a78a45c12e8fc181639873fdfea6440595eb
ayala <balbunho@gmail.com>
2020-06-13 02:12:29 +0200

    B_JV_IA_finish

--- jvideo.js
+++ jvideo.js
@@ -1,5 +1,7 @@
-            
-               
+
+
+
+           
 var i = 0;
 var pos = ["translateY(-0%)","translateY(-100%)","translateY(-200%)","translateY(-300%)"];                          
 var a = 0;
@@ -7,7 +9,7 @@ var transition_1 = document.getElementById('transition');
 var bouton =  document.getElementById("next");
 var bouton_2 =  document.getElementById("previous");
 var bouton_3 = document.getElementById('reload_button');
-var scrolling_2 = document.getElementById('scroll_jv');
+var scrolling_2 = document.getElementById('scroll');
 var translate = ["translateY(0%)" , "translateY(-100%)" , "translateY(-200%)" , "translateY(-300%)"];
 var point_one = document.getElementById("point");
 var point_two = document.getElementById("point2" );
@@ -17,9 +19,6 @@ var point_jor=[point_one , point_two , point_three , point_four ];
 var button_nav_1 = document.getElementById('nav_1');
 var button_nav_2 = document.getElementById('nav_2');
 var button_nav_3 = document.getElementById('nav_3');
-var background_color = [background_color_btc, background_color_btc_2 , background_color_btc_3, background_color_btc_4];
-var lecture_2 = document.getElementById("content_lecture_2");
-var lecture_1 = document.getElementById("content_lecture");
 var transition_2 = document.getElementById('transition_page_1');
 var transition_3 = document.getElementById('transition_page_2');    
 
@@ -46,7 +45,7 @@ var transition_3 = document.getElementById('transition_page_2');
             point_four.style.transition = "none"; 
             bouton.style.transition = "none"; 
             bouton_2.style.transition = "none"; 
-              transition_1.style.transition="0.2s";
+            transition_1.style.transition="0.2s";
             
              setTimeout(function(){ 
              transition_1.style.opacity="0";
@@ -98,106 +97,6 @@ var transition_3 = document.getElementById('transition_page_2');
             
         };
     
-            
-             button_nav_1.addEventListener('click', function (){
-                 
-                          button_nav_1.style.transform = "translateY(-120%)"; 
-                          button_nav_2.style.transform = "translateY(-120%)"; 
-                          button_nav_3.style.transform = "translateY(-120%)"; 
-                          bouton_3.style.transform = "translateY(-150%)";
-                          point_one.style.transform = "translatey(1000%)";
-                          point_two.style.transform = "translatey(1000%)";
-                          point_three.style.transform = "translatey(1000%)";
-                          point_four.style.transform = "translatey(1000%)";
-                          bouton.style.transform = "translatey(200%)";
-                          bouton_2.style.transform = "translatey(200%)";
-                          transition_1.style.zIndex="1000";
-                          
-                     
-                          
-                 
-                           setTimeout(function(){
-                           
-                            transition_1.style.opacity="1";
-                               
-                           },550);
-                         
-                               
-                        setTimeout(function(){
-                                     
-                                     window.location= "index.html";                                     
-                                         
-                                        },1000);
-                
-             
-             });
-            
-             button_nav_2.addEventListener('click', function (){
-                 
-                           button_nav_1.style.transform = "translateY(-120%)"; 
-                          button_nav_2.style.transform = "translateY(-120%)"; 
-                          button_nav_3.style.transform = "translateY(-120%)"; 
-                          bouton_3.style.transform = "translateY(-150%)";
-                          point_one.style.transform = "translatey(1000%)";
-                          point_two.style.transform = "translatey(1000%)";
-                          point_three.style.transform = "translatey(1000%)";
-                          point_four.style.transform = "translatey(1000%)";
-                          bouton.style.transform = "translatey(200%)";
-                          bouton_2.style.transform = "translatey(200%)";
-                          transition_1.style.zIndex="1000";
-                          
-                     
-                          
-                 
-                           setTimeout(function(){
-                           
-                            transition_1.style.opacity="1";
-                               
-                           },550);
-                         
-                 
-                   setTimeout(function(){
-                       
-                                     
-                                     window.location= "ia.html";                                     
-                                         
-                                        },1000);
-             
-             });
-            
-            
-             button_nav_3.addEventListener('click', function (){
-                 
-                         button_nav_1.style.transform = "translateY(-120%)"; 
-                          button_nav_2.style.transform = "translateY(-120%)"; 
-                          button_nav_3.style.transform = "translateY(-120%)"; 
-                          bouton_3.style.transform = "translateY(-150%)";
-                          point_one.style.transform = "translatey(1000%)";
-                          point_two.style.transform = "translatey(1000%)";
-                          point_three.style.transform = "translatey(1000%)";
-                          point_four.style.transform = "translatey(1000%)";
-                          bouton.style.transform = "translatey(200%)";
-                          bouton_2.style.transform = "translatey(200%)";
-                       transition_1.style.zIndex="1000";
-                          
-                     
-                          
-                 
-                           setTimeout(function(){
-                           
-                            transition_1.style.opacity="1";
-                               
-                           },550);
-                         
-                 
-                   setTimeout(function(){
-                                     
-                                     window.location= "blockchain.html";                                     
-                                         
-                                        },1000);
-             
-             });
-                           
             
             
                  bouton.addEventListener('click', function(){
@@ -394,7 +293,7 @@ var transition_3 = document.getElementById('transition_page_2');
            
              if(a==0){
                
-               document.getElementById("scroll_jv").style.transform=pos[a]; 
+               document.getElementById("scroll").style.transform=pos[a]; 
              
                console.log(a)
                  
@@ -406,7 +305,7 @@ var transition_3 = document.getElementById('transition_page_2');
            
            else if(a==1){
                
-               document.getElementById("scroll_jv").style.transform=pos[a]; 
+               document.getElementById("scroll").style.transform=pos[a]; 
                console.log(a)
                   window.removeEventListener("mousewheel",myfunction);
                setTimeout(function(){
@@ -418,7 +317,7 @@ var transition_3 = document.getElementById('transition_page_2');
            
              else if(a==2){
       
-                 document.getElementById("scroll_jv").style.transform=pos[a]; 
+                 document.getElementById("scroll").style.transform=pos[a]; 
                  console.log(a)
                    window.removeEventListener("mousewheel",myfunction);
                setTimeout(function(){
@@ -426,7 +325,7 @@ var transition_3 = document.getElementById('transition_page_2');
                }
                 else if(a==3){
       
-                 document.getElementById("scroll_jv").style.transform=pos[a]; 
+                 document.getElementById("scroll").style.transform=pos[a]; 
                  console.log(a)
                    window.removeEventListener("mousewheel",myfunction);
                setTimeout(function(){
@@ -436,7 +335,8 @@ var transition_3 = document.getElementById('transition_page_2');
             }
              
        
-        
+ 
+
                                    
                  
            
